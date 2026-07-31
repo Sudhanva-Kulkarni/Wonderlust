a2d713e7fe15ee705f56f2bca4db730c11a3bd83
Sudhanva-Kulkarni <sudhanvakulkarni1305@gmail.com>
2026-07-31 11:36:22 +0530

    changes

--- init/index.js
+++ init/index.js
@@ -1,3 +1,6 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const initData = require("./data");
@@ -7,14 +10,14 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+  await mongoose.connect(process.env.ATLAS_URL);
 }
 
 const initDB = async()=>{
     await Listing.deleteMany({});
     initData.data = initData.data.map((obj)=>({
         ...obj,
-        owner: "6a59f7792ff0fa269b181dd4"
+        owner: "6a6c384d3e89e82289102c40"
     }));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized successfully");
