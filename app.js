f91b82a1e5d64e80231e81a2100cb8d734267b52
Sudhanva-Kulkarni <sudhanvakulkarni1305@gmail.com>
2026-07-31 12:09:32 +0530

    rerouted root page

--- app.js
+++ app.js
@@ -74,10 +74,9 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// //root route
-// app.get("/", (req,res)=>{
-//     res.send("root working");
-// })
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
